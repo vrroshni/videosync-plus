c4e464f3e693a949670a84ff117a15079e81d3a8
Roshni V R <vrroshni2001@gmail.com>
2023-09-29 22:37:03 +0530

    client ready to deploy

--- client/src/config/axios/index.js
+++ client/src/config/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // baseURL: import.meta.env.VITE_API_BASE_URL,
-export const API_URL='http://127.0.0.1:8000/'
-export const IMAGE_API_URL='http://127.0.0.1:8000'
+export const API_URL='http://52.195.229.198/'
+export const IMAGE_API_URL='http://52.195.229.198'
 
 const axiosClient = axios.create({
   baseURL: API_URL,
