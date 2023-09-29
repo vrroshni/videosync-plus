ba2eb6a321a4d64a1d685d5b58dd52b7908f383c
Roshni V R <vrroshni2001@gmail.com>
2023-09-29 22:30:14 +0530

    ready to deploy

--- server/server/settings.py
+++ server/server/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tg!w@1=t3@s_twa^0nudh3+eyo(29@(d*-o^$0!5ki__*ae9px
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 CORS_ALLOWED_ORIGINS = [
