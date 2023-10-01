372fe28be7388769ee0dd88883c82c462b8fcb49
Roshni V R <vrroshni2001@gmail.com>
2023-10-01 10:16:18 +0530

    minor changes

--- client/src/components/skeletons/VideosViewSkeleton.vue
+++ client/src/components/skeletons/VideosViewSkeleton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center">
     <div class="flex flex-col md:flex-row flex-wrap  justify-center items-center w-full">
-      <div v-for="i in [1, 2]" :key="i"
+      <div v-for="i in [1, 2,3,4]" :key="i"
         class="overflow-hidden bg-white w-3/4 md:w-1/3  shadow-xl m-4  rounded-lg py-4 px-4  flex flex-col justify-center  min-h-[20rem] gap-3 ">
         <AnimatedPlaceHolder class="h-40  w-full overflow-hidden rounded-lg  "/>
         <AnimatedPlaceHolder class="h-10  w-full overflow-hidden "/>
